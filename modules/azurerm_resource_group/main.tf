42c9a0532cdc3055d669495bab31a58f67004eca
Gaurav <gaurav.shivchauhan@gmail.com>
2025-07-05 17:44:17 +0530

    Added one new resources

--- modules/azurerm_resource_group/main.tf
+++ modules/azurerm_resource_group/main.tf
@@ -3,3 +3,7 @@ resource "azurerm_resource_group" "resource_group" {
   location = var.resource_group_location
 }
 
+resource "azurerm_resource_group" "resource_group2" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
--- todoapp_infra/main.tf
+++ todoapp_infra/main.tf
@@ -4,6 +4,11 @@ module "resource_group" {
   resource_group_location = "centralindia"
 }
 
+module "resource_group2" {
+  source                  = "../modules/azurerm_resource_group"
+  resource_group_name     = "rg-india"
+  resource_group_location = "centralindia"
+}
 
 module "virtual_network" {
   depends_on               = [module.resource_group]
